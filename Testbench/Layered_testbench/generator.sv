729de235c32a97b3fac9206f9fe09c2784e8a07c
Tanvir500 <75424486+Tanvir500@users.noreply.github.com>
2025-09-26 12:24:35 +0600

    Create generator.sv

---
+++ Testbench/Layered_testbench/generator.sv
@@ -0,0 +1,20 @@
+‘include "transaction.sv" 
+ 
+class generator; 
+  mailbox gen2driv; 
+  transaction g_trans; 
+   
+  function new(mailbox gen2driv); 
+    this.gen2driv=gen2driv; 
+  endfunction 
+   
+  task main(input int count); 
+    repeat(count) begin 
+      g_trans=new(); 
+      g_trans=new g_trans;  
+      assert(g_trans.randomize()); 
+      gen2driv.put(g_trans); 
+    end 
+  endtask:main 
+   
+endclass:generator 
